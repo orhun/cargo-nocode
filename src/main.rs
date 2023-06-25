96274b3e5726b85ad2f38f74829c131d2e636391
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-06-25 16:44:36 +0300

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,40 @@
-fn main() {}
+use std::env;
+use std::process;
+
+const USAGE_TEXT: &str = "
+Usage: cargo-nocode <command>
+
+Possible commands:
+- init    initialize a nocode applicaton
+- build   build the nocode application
+- run     run the nocode application
+- deploy  deploy the nocode application
+";
+
+fn usage() {
+    println!("No command given.");
+    println!("{USAGE_TEXT}");
+    process::exit(1)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        usage()
+    }
+
+    let command = &args[1];
+    match command.as_ref() {
+        "init" => {
+            println!("Init command executed");
+        }
+        "build" => {
+            println!("Build command executed");
+        }
+        "deploy" => {
+            println!("Deploy command executed");
+        }
+        _ => usage(),
+    }
+}
